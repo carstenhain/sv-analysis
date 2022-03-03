d726fae75308eda7b1f833bd2317047cd0a6eb51
Carsten <Carstenmhain@gmail.com>
2022-03-03 14:09:47 +0100

    .

--- SV_Mechanism/BuildSVAndTestData.py
+++ SV_Mechanism/BuildSVAndTestData.py
@@ -1,7 +1,38 @@
 import General.LoadData
 import General.ChromosomeMetrics
 
-def build_BED_around_SV_breakpoints (sv_bed_file_path):
+def build_BED_around_SV_breakpoints (sv_bed_file_path, padded_bed_file_path, include_templates, padding):
     sv_bed_data = General.LoadData.load_sv_bed(sv_bed_file_path)
+    fo_padded_bed_file = open(padded_bed_file_path, "w")
 
-    print(sv_bed_data)
+    for sv_id in sv_bed_data:
+        for breakpoint in sv_bed_data[sv_id]:
+            include_sv = True
+            if (not include_templates) and breakpoint[2].startswith("template"):
+                include_sv = False
+
+            if include_templates:
+                fo_padded_bed_file.write("\t".join(
+                    [
+                        breakpoint[0],
+                        str(breakpoint[1] - padding),
+                        str(breakpoint[1] + padding)
+                    ]
+                ) + "\n")
+
+    fo_padded_bed_file.flush()
+def build_BED_around_random_breakpoints (n_points, padded_bed_file_path, include_templates, padding):
+    sv_bed_data = General.LoadData.load_sv_bed(sv_bed_file_path)
+    fo_padded_bed_file = open(padded_bed_file_path, "w")
+
+    for i in range(0, n_points):
+        chrom, pos = General.ChromosomeMetrics.get_random_position("..\\Resources\\karyotype.human.hg38.txt")
+        fo_padded_bed_file.write("\t".join(
+            [
+                chrom,
+                str(pos - padding),
+                str(pos + padding)
+            ]
+        ) + "\n")
+
+    fo_padded_bed_file.flush()
--- SV_Mechanism/Main_SV_Mechanism.py
+++ SV_Mechanism/Main_SV_Mechanism.py
@@ -1,4 +1,3 @@
 import BuildSVAndTestData
 
-
-BuildSVAndTestData.
+BuildSVAndTestData.build_BED_around_SV_breakpoints("C:\\Users\\Carst\\Documents\\Promotion\\MF_Verlaufspatient\\WGS\\SV\\CD3Iso.Manuell.Truth_Set.plus.Add_In.templates.manuell_kontrolliert.Final.Sortiert.bed")
