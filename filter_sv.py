56e735beccdc4e69ab5ef751a1ada5aa99fb5c06
carstenhain <100699148+carstenhain@users.noreply.github.com>
2023-02-06 13:52:57 +0100

    Update filter_sv.py

--- filter_sv.py
+++ filter_sv.py
@@ -4,63 +4,65 @@ from time import time, ctime, time_ns
 import germline_filter
 import os
 
-parser = argparse.ArgumentParser(description='Filters false positive structural variants from a vcf file')
-parser.add_argument("--vcf", required=True, help="Path to SV vcf file")
-parser.add_argument("--bam", required=True, help="Path to corresponding BAM file")
-parser.add_argument("--ref", required=True, help="Path to reference FASTA file")
-parser.add_argument("--folder", required=True, help="Path to data folder")
+def map_reads_assemblies (reference, folder):
+    os.system("cat " + folder + "sv_fastq/*.fastq > tmp.fastq")
+    os.system("minimap2 -ax map-ont -t 26 " + reference + " tmp.fastq -Y | samtools view -b - | samtools sort > " + folder + "sv_mappings/sv_reads_to_reference.bam")
+    os.system("samtools index " + folder + "sv_mappings/sv_reads_to_reference.bam")
 
-args = parser.parse_args()
+    os.system("cat " + folder + "sv_assemblies/*.fasta > tmp.fasta")
+    os.system("minimap2 -ax map-ont -t 26 " + reference + " tmp.fasta -Y | samtools view -b - | samtools sort > " + folder + "sv_mappings/sv_assemblies_to_reference.bam")
+    os.system("samtools index " + folder + "sv_mappings/sv_assemblies_to_reference.bam")
 
-data_folder = args.folder
-if not data_folder.endswith("/"):
-    data_folder += "/"
+    os.system("cat " + folder + "germline_fastq/*.fastq > tmp.fastq")
+    os.system("minimap2 -ax map-ont -t 26 " + reference + " tmp.fastq -Y | samtools view -b - | samtools sort > " + folder + "germline_mappings/germline_reads_to_reference.bam")
+    os.system("samtools index " + folder + "sv_mappings/sv_reads_to_reference.bam")
 
-print("Starting Filter SV at " + ctime(time()))
-print("Starting the prefilter module at " + ctime(time()))
-time_ns_prefilter_start = time_ns()
-total_variants, short_variants, filtered_variants, passing_variants = prefilter.prefilter(args.vcf, args.bam, data_folder)
-time_ns_prefilter_end = time_ns()
-print("Completing the prefilter module at " + ctime(time()) + ", elapsed time " + str(round((time_ns_prefilter_end - time_ns_prefilter_start) / 1000000000, 3)) + " s")
-print("\tTotal variants\t\t" + str(total_variants))
-print("\tShort variants\t\t" + str(short_variants))
-print("\tFiltered variants\t" + str(filtered_variants))
-print("\tPassing variants\t" + str(passing_variants))
+    os.system("cat " + folder + "germline_assemblies/*.fasta > tmp.fasta")
+    os.system("minimap2 -ax map-ont -t 26 " + reference + " tmp.fasta -Y | samtools view -b - | samtools sort > " + folder + "germline_mappings/germline_assemblies_to_reference.bam")
+    os.system("samtools index " + folder + "germline_mappings/germline_assemblies_to_reference.bam")
 
-print("Starting the germline insertion module at " + ctime(time()))
-time_ns_germlinefilter_start = time_ns()
-germline_filter.germline_filter (
-    args.ref,
-    args.vcf.split(".vcf")[0].split("/")[-1] + ".prefilter.vcf",
-    args.bam,
-    args.folder)
-time_ns_germlinefilter_end = time_ns()
-print("Completing the germline insertion module at " + ctime(time()) + ", elapsed time " + str(round((time_ns_germlinefilter_end - time_ns_germlinefilter_start) / 1000000000, 3)))
+    os.system("rm tmp.fastq")
+    os.system("rm tmp.fasta")
 
-# map to reference
-print("Mapping reads and assemblies to the reference genome at " + ctime(time()))
-time_ns_mapping_start = time_ns()
-
-os.system("cat " + args.folder + "sv_fastq/*.fastq > tmp.fastq")
-os.system("minimap2 -ax map-ont -t 26 " + args.ref + " tmp.fastq -Y | samtools view -b - | samtools sort > " + args.folder + "sv_mappings/sv_reads_to_reference.bam")
-os.system("samtools index " + args.folder + "sv_mappings/sv_reads_to_reference.bam")
-
-os.system("cat " + args.folder + "sv_assemblies/*.fasta > tmp.fasta")
-os.system("minimap2 -ax map-ont -t 26 " + args.ref + " tmp.fasta -Y | samtools view -b - | samtools sort > " + args.folder + "sv_mappings/sv_assemblies_to_reference.bam")
-os.system("samtools index " + args.folder + "sv_mappings/sv_assemblies_to_reference.bam")
-
-os.system("cat " + args.folder + "germline_fastq/*.fastq > tmp.fastq")
-os.system("minimap2 -ax map-ont -t 26 " + args.ref + " tmp.fastq -Y | samtools view -b - | samtools sort > " + args.folder + "germline_mappings/germline_reads_to_reference.bam")
-os.system("samtools index " + args.folder + "sv_mappings/sv_reads_to_reference.bam")
-
-os.system("cat " + args.folder + "germline_assemblies/*.fasta > tmp.fasta")
-os.system("minimap2 -ax map-ont -t 26 " + args.ref + " tmp.fasta -Y | samtools view -b - | samtools sort > " + args.folder + "germline_mappings/germline_assemblies_to_reference.bam")
-os.system("samtools index " + args.folder + "germline_mappings/germline_assemblies_to_reference.bam")
-
-os.system("rm tmp.fastq")
-os.system("rm tmp.fasta")
-
-time_ns_mapping_end = time_ns()
-print("Completing the mapping step at at " + ctime(time()) + ", elapsed time " + str(round((time_ns_mapping_end - time_ns_mapping_start) / 1000000000, 3)))
-
-
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Filters false positive structural variants from a vcf file')
+    parser.add_argument("--vcf", required=True, help="Path to SV vcf file")
+    parser.add_argument("--bam", required=True, help="Path to corresponding BAM file")
+    parser.add_argument("--ref", required=True, help="Path to reference FASTA file")
+    parser.add_argument("--folder", required=True, help="Path to data folder")
+    
+    args = parser.parse_args()
+    
+    data_folder = args.folder
+    if not data_folder.endswith("/"):
+        data_folder += "/"
+    
+    print("Starting Filter SV at " + ctime(time()))
+    print("Starting the prefilter module at " + ctime(time()))
+    time_ns_prefilter_start = time_ns()
+    total_variants, short_variants, filtered_variants, passing_variants = prefilter.prefilter(args.vcf, args.bam, data_folder)
+    time_ns_prefilter_end = time_ns()
+    print("Completing the prefilter module at " + ctime(time()) + ", elapsed time " + str(round((time_ns_prefilter_end - time_ns_prefilter_start) / 1000000000, 3)) + " s")
+    print("\tTotal variants\t\t" + str(total_variants))
+    print("\tShort variants\t\t" + str(short_variants))
+    print("\tFiltered variants\t" + str(filtered_variants))
+    print("\tPassing variants\t" + str(passing_variants))
+    
+    print("Starting the germline insertion module at " + ctime(time()))
+    time_ns_germlinefilter_start = time_ns()
+    germline_filter.germline_filter (
+        args.ref,
+        args.vcf.split(".vcf")[0].split("/")[-1] + ".prefilter.vcf",
+        args.bam,
+        args.folder)
+    time_ns_germlinefilter_end = time_ns()
+    print("Completing the germline insertion module at " + ctime(time()) + ", elapsed time " + str(round((time_ns_germlinefilter_end - time_ns_germlinefilter_start) / 1000000000, 3)))
+    
+    # map to reference
+    print("Mapping reads and assemblies to the reference genome at " + ctime(time()))
+    time_ns_mapping_start = time_ns()
+    map_reads_assemblies (args.ref, args.folder)
+    time_ns_mapping_end = time_ns()
+    print("Completing the mapping step at at " + ctime(time()) + ", elapsed time " + str(round((time_ns_mapping_end - time_ns_mapping_start) / 1000000000, 3)))
+    
